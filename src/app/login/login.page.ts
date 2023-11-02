9d64fc753bcf78d9e1a03c41c740a443831c1d04
Brasther <sebastiansalamanca274@gmail.com>
2023-11-01 22:17:29 -0300

    RegistroClase

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -18,7 +18,7 @@ export class LoginPage implements OnInit {
     public alertController: AlertController
   ) { 
     this.formularioLogin = this.fb.group({
-      'nombre': new FormControl("", [Validators.required, Validators.pattern(/^[a-zA-Z0-9]{3,8}$/)]),
+      'usuario': new FormControl("", [Validators.required, Validators.pattern(/^[a-zA-Z0-9]{3,8}$/)]),
       'password': new FormControl("", [Validators.required, Validators.pattern(/^\d{4}$/)])
     });
   }
@@ -28,37 +28,39 @@ export class LoginPage implements OnInit {
   async ingresar() {
     if (this.formularioLogin.valid) {
       const f = this.formularioLogin.value;
-
-      // Obtén la lista de usuarios registrados desde el localStorage
-      const usuariosJSON = await Preferences.get({ key: 'usuarios'});
-      const usuarios: { nombre: string, password: string} [] = usuariosJSON && usuariosJSON.value ? JSON.parse(usuariosJSON.value) : [];
-      // Busca al usuario por su nombre de usuario
-      const user = usuarios.find((u: any) => u.usuario === f.nombre && u.password === f.password);
-
-        if (user) {
-          // Compara el nombre de usuario y la contraseña ingresados
-          await Preferences.set({ key: 'nombreUsuario', value: user.nombre});
-          await Preferences.set({ key: 'usuario', value: JSON.stringify(usuarios)});
-          console.log("Sesión iniciada");
-          this.router.navigate(['/home']);
-          }else{
-            const alert = await this.alertController.create({
-              header: 'Datos incorrectos',
-              message: 'Los datos que se ingresaron no son correctos.',
-              buttons: ['Aceptar']
-            });
-      
-            await alert.present();
-          } 
-
-      }else {
+  
+      // Obtén la lista de usuarios registrados desde Preferences
+      const usuariosJSON = await Preferences.get({ key: 'usuarios' });
+      const usuarios: { usuario: string, password: string }[] = usuariosJSON && usuariosJSON.value ? JSON.parse(usuariosJSON.value) : [];
+  
+      // Busca al usuario por su nombre de usuario (campo 'usuario')
+      const user = usuarios.find((u: any) => u.usuario === f.usuario && u.password === f.password);
+  
+      if (user) {
+        // Guarda los datos del usuario que ha iniciado sesión
+        await Preferences.set({ key: 'nombreUsuario', value: user.usuario });
+        await Preferences.set({ key: 'usuario', value: JSON.stringify(user)  });
+  
+        console.log("Sesión iniciada");
+        this.router.navigate(['/home']);
+      } else {
+        const alert = await this.alertController.create({
+          header: 'Datos incorrectos',
+          message: 'Los datos que se ingresaron no son correctos.',
+          buttons: ['Aceptar']
+        });
+  
+        await alert.present();
+      }
+    } else {
       const alert = await this.alertController.create({
         header: 'Error',
         message: 'Por favor, complete todos los campos correctamente.',
         buttons: ['Aceptar']
       });
-
+  
       await alert.present();
     }
   }
+  
 }
--- src/app/registro-clase/registro-clase.page.ts
+++ src/app/registro-clase/registro-clase.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Geolocation, GeolocationPosition } from '@capacitor/geolocation';
 import { Preferences } from '@capacitor/preferences';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-registro-clase',
   templateUrl: './registro-clase.page.html',
@@ -15,15 +16,21 @@ export class RegistroClasePage implements OnInit {
   horaDesdePreferences: string | null = '';
   usuarios: any[] = [];
   imageSrc: string = '';
-  constructor() { }
+
+
+  usuarioSesion: any; // Para almacenar los datos del usuario que ha iniciado sesión
+  fotoTomada: boolean = false; // Variable para verificar si se ha tomado una foto
+  constructor(private router: Router,private alertController: AlertController) { }
 
   ngOnInit() {
     this.getCurrentPosition();
     this.retrieveMessageFromPreferences();
     this.retrieveUsuariosFromPreferences();
     this.retrieveHoraFromPreferences();
+    this.retrieveUsuarioSesion(); // Agrega esta línea
+
   }
-  
+
   async getCurrentPosition() {
     try {
       const coordinates: GeolocationPosition = await Geolocation.getCurrentPosition();
@@ -39,11 +46,12 @@ export class RegistroClasePage implements OnInit {
       const image = await Camera.getPhoto({
         quality: 90,
         allowEditing: true,
-        resultType: CameraResultType.Uri
+        resultType: CameraResultType.Uri,
       });
 
       if (image && image.webPath) {
         this.imageSrc = image.webPath;
+        this.fotoTomada = true; // Marca la foto como tomada
       } else {
         console.log('No se capturó ninguna imagen o la imagen es nula.');
       }
@@ -52,14 +60,12 @@ export class RegistroClasePage implements OnInit {
     }
   }
 
-
   async retrieveMessageFromPreferences() {
     try {
       const mensaje = await Preferences.get({ key: 'mensaje' });
       if (mensaje && mensaje.value) {
         this.mensajeDesdePreferences = this.agregarSaltosDeLinea(mensaje.value);
-        console.log("ahora si ",mensaje);
-        
+        console.log('ahora si ', mensaje);
       }
     } catch (error) {
       console.error('Error al recuperar el mensaje desde Preferences:', error);
@@ -77,6 +83,51 @@ export class RegistroClasePage implements OnInit {
     }
   }
 
+  
+
+
+
+
+
+  async retrieveUsuarioSesion() {
+    try {
+      const usuarioJSON = await Preferences.get({ key: 'usuario' });
+      if (usuarioJSON && usuarioJSON.value) {
+        this.usuarioSesion = JSON.parse(usuarioJSON.value);
+      }
+    } catch (error) {
+      console.error('Error al recuperar el usuario de la sesión desde Preferences:', error);
+    }
+  }
+
+  async registrarClase() {
+    if (this.fotoTomada) {
+      // Realiza el proceso de registro de la clase aquí
+      // Puedes agregar tu lógica para el registro de la clase
+      // Si todo va bien, muestra un mensaje de éxito
+      const alert = await this.alertController.create({
+        header: 'Registro exitoso',
+        message: 'La clase ha sido registrada correctamente.',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+    } else {
+      // Muestra una alerta si no se ha tomado una foto
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Debes tomar una foto antes de registrar la clase.',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+    }
+}
+
+
+
+
+
   async retrieveHoraFromPreferences() {
     try {
       const hora = await Preferences.get({ key: 'hora' });
@@ -90,5 +141,21 @@ export class RegistroClasePage implements OnInit {
 
   agregarSaltosDeLinea(mensaje: string) {
     return mensaje.replace(/,/g, '<br>');
+  }
+
+  async cerrarSesion() {
+    // Elimina los datos relacionados con la sesión actual del LocalStorage
+    await Preferences.remove({ key: 'nombreUsuario' });
+    await Preferences.remove({ key: 'usuario' });
+
+    // Elimina los datos relacionados con mensaje y hora
+    await Preferences.remove({ key: 'mensaje' });
+    await Preferences.remove({ key: 'hora' });
+
+    // Limpia la imagen
+    this.imageSrc = ''; // Establece la imagen en blanco
+    this.fotoTomada=false;
+    console.log('Sesión cerrada');
+    this.router.navigate(['/login']); // Redirige al usuario a la página de inicio de sesión
   }
 }
